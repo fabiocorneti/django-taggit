2720de15d0a5759a05a9c866d1b68af72ebd69ec
Fabio Corneti <info@corneti.com>
2010-03-31 14:28:37 +0200

    Hide inlines for now, they should be readonly links imho

--- taggit/admin.py
+++ taggit/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
-
 from taggit.models import Tag, TaggedItem
 
-
-class TaggedItemInline(admin.StackedInline):
+class TaggedItemInline(admin.TabularInline):
     model = TaggedItem
+    list_display = ['get_absolute_url',]
 
 class TagAdmin(admin.ModelAdmin):
-    inlines = [
-        TaggedItemInline
-    ]
-
+    pass
 
 admin.site.register(Tag, TagAdmin)
