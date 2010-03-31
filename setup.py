9584212d58ad6cc77876f80bb943fdfdcaed5974
Fabio Corneti <info@corneti.com>
2010-03-31 11:22:27 +0200

    Marking as not zip_safe for easier integration under different virtualenv/buildout setups

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     author_email='alex.gaynor@gmail.com',
     url='http://github.com/alex/django-taggit/tree/master',
     packages=find_packages(),
+    zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
