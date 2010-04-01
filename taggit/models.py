37386deb997663a1a086c0e5505b53beb530c766
Fabio Corneti <info@corneti.com>
2010-04-01 17:21:30 +0200

    Introduced TagContext model

--- taggit/models.py
+++ taggit/models.py
@@ -3,6 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.generic import GenericForeignKey
 from django.db import models, IntegrityError
 from django.template.defaultfilters import slugify
+from django.utils import translation
 from django.utils.translation import ugettext_lazy as _
 
 try:
@@ -10,6 +11,64 @@ try:
     TRANSMETA_AVAILABLE = True
 except:
     TRANSMETA_AVAILABLE = False
+
+class TagContext(models.Model):
+    """
+    A tag context.
+    
+    Contexts can be used to categorize tags; for example, tags representing 
+    colors could be associated to a Color context to generate navigation 
+    hierarchies or filters::
+    
+    TagContext     Color
+        Tag            Red
+        Tag            Green
+        Tag            Blue
+    
+    They can also be used to resolve tag ambiguity::
+    
+    TagContext     Car
+        Tag            Jaguar
+
+    TagContext     Animal
+        Tag            Jaguar
+
+    TagContext     Car
+        Tag            Jaguar
+
+    TagContext     Animal
+        Tag            Jaguar
+    """
+    if TRANSMETA_AVAILABLE:
+        __metaclass__ = TransMeta
+
+    name = models.CharField(max_length=255, verbose_name=_(u"Name"))
+    slug = models.SlugField(unique=True, max_length=255, verbose_name=_(u"Slug"))
+    
+    class Meta:
+        verbose_name = _(u"Tag context")
+        verbose_name_plural = _(u"Tag contexts")
+        if TRANSMETA_AVAILABLE:
+            translate = ('name',)
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.pk and not self.slug:
+            if TRANSMETA_AVAILABLE:
+                self.slug = slug = slugify(getattr(self, 'name_' + translation.get_language().lower()))
+            else:
+                self.slug = slug = slugify(self.name)
+            i = 0
+            while True:
+                try:
+                    return super(TagContext, self).save(*args, **kwargs)
+                except IntegrityError:
+                    i += 1
+                    self.slug = "%s_%d" % (slug, i)
+        else:
+            return super(TagContext, self).save(*args, **kwargs)
 
 class Tag(models.Model):
     if TRANSMETA_AVAILABLE:
