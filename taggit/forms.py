4f7fa296f214dc258280cd0a7fbe94dc23f6c1b8
Fabio Corneti <info@corneti.com>
2010-04-01 17:22:17 +0200

    Display full tag (with context) in TagField

--- taggit/forms.py
+++ taggit/forms.py
@@ -6,7 +6,7 @@ from taggit.utils import parse_tags
 class TagWidget(forms.TextInput):
     def render(self, name, value, attrs=None):
         if value is not None and not isinstance(value, basestring):
-            value = ", ".join(o.tag.name for o in value.select_related("tag"))
+            value = ", ".join(unicode(o.tag) for o in value.select_related("tag"))
         return super(TagWidget, self).render(name, value, attrs)
 
 class TagField(forms.CharField):
